b04b440d171f248dfbf4ee529d9218439f694720
Vishal Kotcherlakota <dishbreak@gmail.com>
2022-03-04 23:41:26 -0800

    day3 completed

---
+++ cmd/day3/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"strconv"
+	"strings"
+
+	"github.com/dishbreak/aoc2020/lib"
+)
+
+func main() {
+	input, err := lib.GetInput("inputs/day3.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
+}
+
+func abs(o int) int {
+	if o > 0 {
+		return o
+	}
+	return -1 * o
+}
+
+func dist(w image.Point) int {
+	return abs(w.X) + abs(w.Y)
+}
+
+var directions = map[byte]image.Point{
+	'R': {1, 0},
+	'L': {-1, 0},
+	'U': {0, 1},
+	'D': {0, -1},
+}
+
+func part1(input []string) int {
+	match := 100000000000
+	board := make(map[image.Point]bool)
+
+	cursor := image.Point{}
+
+	for _, inst := range strings.Split(input[0], ",") {
+		vector := directions[inst[0]]
+		mag, _ := strconv.Atoi(inst[1:])
+		for i := 0; i < mag; i++ {
+			board[cursor] = true
+			cursor = cursor.Add(vector)
+		}
+	}
+
+	cursor.X, cursor.Y = 0, 0
+
+	for _, inst := range strings.Split(input[1], ",") {
+		vector := directions[inst[0]]
+		mag, _ := strconv.Atoi(inst[1:])
+		for i := 0; i < mag; i++ {
+			if d := dist(cursor); board[cursor] && d != 0 {
+				if d < match {
+					match = d
+				}
+			}
+			cursor = cursor.Add(vector)
+		}
+	}
+
+	return match
+}
+
+func part2(input []string) int {
+	match := 100000000000
+	board := make(map[image.Point]int)
+
+	cursor := image.Point{}
+
+	step := 0
+	for _, inst := range strings.Split(input[0], ",") {
+		vector := directions[inst[0]]
+		mag, _ := strconv.Atoi(inst[1:])
+		for i := 0; i < mag; i++ {
+			board[cursor] = step
+			cursor = cursor.Add(vector)
+			step++
+		}
+	}
+
+	cursor.X, cursor.Y = 0, 0
+	step = 0
+	for _, inst := range strings.Split(input[1], ",") {
+		vector := directions[inst[0]]
+		mag, _ := strconv.Atoi(inst[1:])
+		for i := 0; i < mag; i++ {
+			if d := dist(cursor); board[cursor] != 0 && d != 0 {
+				t := board[cursor] + step
+				if t < match {
+					match = t
+				}
+			}
+			cursor = cursor.Add(vector)
+			step++
+		}
+	}
+
+	return match
+}
---
+++ cmd/day3/main_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1(t *testing.T) {
+	type testCase struct {
+		input    []string
+		expected int
+	}
+
+	testCases := []testCase{
+		{
+			input: []string{
+				"R8,U5,L5,D3",
+				"U7,R6,D4,L4",
+				"",
+			},
+			expected: 6,
+		},
+		{
+			input: []string{
+				"R75,D30,R83,U83,L12,D49,R71,U7,L72",
+				"U62,R66,U55,R34,D71,R55,D58,R83",
+			},
+			expected: 159,
+		},
+		{
+			input: []string{
+				"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+				"U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+			},
+			expected: 135,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			actual := part1(tc.input)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	type testCase struct {
+		input    []string
+		expected int
+	}
+
+	testCases := []testCase{
+		{
+			input: []string{
+				"R8,U5,L5,D3",
+				"U7,R6,D4,L4",
+				"",
+			},
+			expected: 30,
+		},
+		{
+			input: []string{
+				"R75,D30,R83,U83,L12,D49,R71,U7,L72",
+				"U62,R66,U55,R34,D71,R55,D58,R83",
+			},
+			expected: 610,
+		},
+		{
+			input: []string{
+				"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+				"U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+			},
+			expected: 410,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			actual := part2(tc.input)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
