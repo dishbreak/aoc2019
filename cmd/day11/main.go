58675f8a285a690904663e31126ce47abb6d9496
Vishal Kotcherlakota <dishbreak@gmail.com>
2023-11-03 00:07:09 -0700

    finished day 11

--- cmd/day11/main.go
+++ cmd/day11/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"image"
+	"math"
+	"strings"
 
 	"github.com/dishbreak/aoc-common/lib"
 	"github.com/dishbreak/aoc2019/util"
@@ -16,6 +18,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input[0]))
+	fmt.Printf("Part 2:\n%s\n", part2(input[0]))
 }
 
 func part1(input string) int {
@@ -77,4 +80,56 @@ func runRobot(ctx context.Context, inputStream chan<- int64, output <-chan int64
 	}()
 
 	return result
+}
+
+func part2(input string) string {
+	pgm, err := util.ParseIntcode(input)
+	if err != nil {
+		panic(fmt.Errorf("failed to parse program: %w", err))
+	}
+
+	comp := &util.IntcodeComputer{}
+	comp.Load(pgm)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	inputStream := make(chan int64, 1)
+	output := make(chan int64)
+	errStream := make(chan error)
+	defer close(inputStream)
+
+	go comp.Simulate(ctx, "hull painter", inputStream, output, errStream)
+	hull := <-runRobot(ctx, inputStream, output, 1)
+
+	minPt := image.Pt(math.MaxInt, math.MaxInt)
+	maxPt := image.Pt(math.MinInt, math.MinInt)
+
+	for p := range hull {
+		if p.X < minPt.X {
+			minPt.X = p.X
+		} else if p.X > maxPt.X {
+			maxPt.X = p.X
+		}
+		if p.Y < minPt.Y {
+			minPt.Y = p.Y
+		} else if p.Y > maxPt.Y {
+			maxPt.Y = p.Y
+		}
+	}
+
+	var sb strings.Builder
+
+	for y := maxPt.Y; y >= minPt.Y; y-- {
+		for x := maxPt.X; x >= minPt.X; x-- {
+			if hull[image.Pt(x, y)] == 0 {
+				sb.WriteRune(' ')
+				continue
+			}
+			sb.WriteRune('#')
+		}
+		sb.WriteRune('\n')
+	}
+
+	return sb.String()
 }
