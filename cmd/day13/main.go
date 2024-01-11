f617408afc0efa9ae4fb3150a5dcb9cf56510c77
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2024-01-11 12:50:02 -0800

    day 13 part 1

---
+++ cmd/day13/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"image"
+
+	"github.com/dishbreak/aoc-common/lib"
+	"github.com/dishbreak/aoc2019/util"
+)
+
+func main() {
+	input, err := lib.GetInput("inputs/day13.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Part 1: %d\n", part1(input[0]))
+}
+
+func part1(input string) int {
+	pgm, err := util.ParseIntcode(input)
+	if err != nil {
+		panic(err)
+	}
+
+	comp := util.IntcodeComputer{}
+	comp.Load(pgm)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	output := make(chan int64)
+	errStream := make(chan error)
+
+	go comp.Simulate(ctx, "brick game", nil, output, errStream)
+
+	gameBoard := make(map[image.Point]int)
+	i := 0
+	tilePt := image.Pt(-1, -1)
+	for o := range output {
+		switch i {
+		case 0: // store the X
+			tilePt.X = int(o)
+		case 1: // store the Y
+			tilePt.Y = int(o)
+		case 2: // process based on id
+			gameBoard[tilePt] = int(o)
+		}
+		i++
+		i = i % 3
+	}
+
+	acc := 0
+	for _, tileId := range gameBoard {
+		if tileId == 2 {
+			acc++
+		}
+	}
+
+	return acc
+}
